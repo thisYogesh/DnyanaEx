83aa1d513f8eac761b5aac0f289be4eda2bb00f8
aarti-git <yog99mail@gmail.com>
2018-09-13 11:27:55 +0530

    created template cache

--- index.js
+++ index.js
@@ -11,21 +11,53 @@ var files = [
     'scriptlet.js',
     'rules.js'
 ];
-var os = fs.openSync(outputPath, 'w');
 
 try {
     let content = "";
+    let templateCache = createTemplateCache();
+
     for (var i = 0; i < files.length; i++) {
-        let result = minifier.minify(fs.readFileSync(`./lib/${files[i]}`, 'utf8'));
+        let result = getFileData(`./lib/${files[i]}`);
         content += `\n/** ===== Start of file ${files[i]} ===== **/\n`;
         content += result.code;
         content += `\n/** ===== End of file ${files[i]} ===== **/\n\n`;
+
+        // putting tamplate cache after template.js
+        if (files[i] == "template.js") {
+            content += `\n/** ===== Start Of Tamplate Cache ===== **/\n`;
+            content += templateCache;
+            content += `\n/** ===== End Of Tamplate Cache ===== **/\n\n`;
+        }
     };
 
-    fs.writeFileSync(outputPath, content, 'utf8');
-    fs.closeSync(os);
-
+    createFile(outputPath, content)
     console.log('\x1b[42m', `${outputPath} created!`, '\x1b[0m')
 } catch (e) {
-    console.log('\x1b[41m', `error in ${e.path}`, '\x1b[0m')
+    console.log('\x1b[41m', `error in ${e}, on ${e.path}`, '\x1b[0m')
+}
+
+function createFile(outputPath, content) {
+    let os = fs.openSync(outputPath, 'w');
+    fs.writeFileSync(outputPath, content, 'utf8');
+    fs.closeSync(os);
+}
+
+function getFileData(path, doMinify = true) {
+    return doMinify ? minifier.minify(fs.readFileSync(path, 'utf8')) : fs.readFileSync(path, 'utf8');
+}
+
+function createTemplateCache() {
+    let files = fs.readdirSync('./templates');
+    let templateCache = ""
+
+    files.forEach(function (file) {
+        let _file = getFileData('./templates/' + file, false)
+        _file = _file.replace(/(\r\n|\n|\r)/g, '').replace(/'/g, "\\'");
+        templateCache += `templates.put('${file}', '${_file}')\n`
+    })
+
+    // createFile("templateCache.js", templateCache);
+    console.log('\x1b[42m', `Template Cache Created!`, '\x1b[0m')
+
+    return templateCache
 }
